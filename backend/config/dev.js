c76bb911d1bbc807b3942ca2deb0961b1073c282
Christopher Smith <atebit-air@macbookair.lan>
2025-02-05 06:56:11 -0600

    change db

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://atebitcreative:bi246oB3QoSH47Wf@donezo.pv02j.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://donezo:bi246oB3QoSH47Wf@donezo.pv02j.mongodb.net/?retryWrites=true&w=majority',
   dbName: 'donezo'
 }
